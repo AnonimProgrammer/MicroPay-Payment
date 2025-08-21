f548585f4c7098adc3bda4a12d500921615376ea
AnonimProgrammer <zephyr11ff@gmail.com>
2025-08-21 23:38:15 +0400

    Fixed issue with ID in Repository interface.

--- src/main/java/com/docker/payment/repo/PaymentRepository.java
+++ src/main/java/com/docker/payment/repo/PaymentRepository.java
@@ -3,7 +3,5 @@ package com.docker.payment.repo;
 import com.docker.payment.model.payment.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
