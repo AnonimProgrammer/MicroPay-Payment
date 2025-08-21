38633ce68f7dc1a165cc166ab6ee6ea3a6578efe
AnonimProgrammer <zephyr11ff@gmail.com>
2025-08-21 23:37:09 +0400

    Exception class used by data access methods.

---
+++ src/main/java/com/docker/payment/exception/PaymentNotFoundException.java
@@ -0,0 +1,11 @@
+package com.docker.payment.exception;
+
+public class PaymentNotFoundException extends RuntimeException {
+    public PaymentNotFoundException(String message) {
+        super(message);
+    }
+
+    public PaymentNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
