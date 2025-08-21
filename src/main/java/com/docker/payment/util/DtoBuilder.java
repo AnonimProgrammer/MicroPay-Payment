3095d3bf677164f570b5f41a8772a2d446317777
AnonimProgrammer <zephyr11ff@gmail.com>
2025-08-21 23:31:04 +0400

    CreditWalletEvent and DebitWalletEvent build methods are added.

--- src/main/java/com/docker/payment/util/DtoBuilder.java
+++ src/main/java/com/docker/payment/util/DtoBuilder.java
@@ -2,10 +2,13 @@ package com.docker.payment.util;
 
 import com.docker.payment.dto.payment.internal.PaymentResponse;
 import com.docker.payment.dto.transaction.InitiateTransactionEvent;
+import com.docker.payment.dto.wallet.credit.CreditWalletEvent;
+import com.docker.payment.dto.wallet.debit.DebitWalletEvent;
 import com.docker.payment.model.payment.PaymentModel;
 
 public class DtoBuilder {
 
+    // TRANSACTION EVENTS
     public static InitiateTransactionEvent buildInitiateTransactionEvent(PaymentModel payment) {
         return new InitiateTransactionEvent.Builder()
                 .amount(payment.getAmount())
@@ -16,6 +19,23 @@ public class DtoBuilder {
                 .type(payment.getType())
                 .paymentId(payment.getId())
                 .build();
+    }
+
+    // WALLET EVENTS
+    public static CreditWalletEvent buildCreditWalletEvent(PaymentModel payment) {
+        return new CreditWalletEvent(
+                payment.getTransactionId(),
+                Long.valueOf(payment.getDestination()),
+                payment.getAmount()
+        );
+    }
+
+    public static DebitWalletEvent buildDebitWalletEvent(PaymentModel payment) {
+        return new DebitWalletEvent(
+                payment.getTransactionId(),
+                Long.valueOf(payment.getSource()),
+                payment.getAmount()
+        );
     }
 
     public static PaymentResponse buildPaymentResponse(PaymentModel payment) {
